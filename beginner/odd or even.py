7f4af7339af4c94c0e2e4e7edb2dac3b1712bab1
gurusankar22 <36753362+gurusankar22@users.noreply.github.com>
2018-02-23 16:04:38 +0530

    Update odd or even.py

--- beginner/odd or even.py
+++ beginner/odd or even.py
@@ -1,6 +1,6 @@
 num = int(input("Enter the number: "))
 mod = num % 2
- if mod > 0:
-Print ("This is an oddnumber.")
-else:
-Print ("This is an evennumber.")
+if mod > 0:
+ print("This is an oddnumber.")
+else: 
+  print("This is an evennumber.")
